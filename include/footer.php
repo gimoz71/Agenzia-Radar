f507f995120683e5913b08a48db18a15c70e6ae3
gimoz71 <gm3871@gmail.com>
2016-02-23 12:17:17 +0100

    fix e tuning footer

--- include/footer.php
+++ include/footer.php
@@ -1,142 +1,146 @@
- <!-- Footer
+
+
+
+<!-- Footer
+============================================= -->
+<footer id="footer" class="dark">
+
+    <div class="container">
+
+        <!-- Footer Widgets
         ============================================= -->
-        <footer id="footer" class="dark">
+        <div class="footer-widgets-wrap clearfix">
 
-            <div class="container">
+            <div class="col_two_third">
 
-                <!-- Footer Widgets
-                ============================================= -->
-                <div class="footer-widgets-wrap clearfix">
+                <div class="widget clearfix">
 
-                    <div class="col_two_third">
-
-                        <div class="widget clearfix">
-
-                            <img src="images/custom/footer-logo.png" alt="Radar logo" class="alignleft" style="margin-top: 8px; padding-right: 18px; border-right: 1px solid #4A4A4A;">
-
-                            <p><strong>Agenzia Radar Immobiliare</strong> - Via Aurelia, 548 - 57016 Castiglioncello (LI)<br>Tel. +39.0586.752596 - Fax +39.0586.759935 - Email: <a href="info@agenziaradar.it">info@agenziaradar.it</a><br>P.iva 01169600499</p>
-
-                            <div class="line" style="margin: 30px 0;"></div>
-
-                            <div class="row">
-
-                                <div class="col-md-3 col-xs-6 bottommargin-sm widget_links">
-                                    <ul>
-                                        <li><a class="home" href="<?=LANFOLDER?>index.php"><?php echo HOME_FOOTER; ?></a></li>
-                                        <li><a href="<?=LANFOLDER?>agenzia_immobiliare_casa_vacanze_toscana.php" title="affitti case vacanze"><?php echo AGENZIA_FOOTER; ?></a></li>
-                                        <li><a href="<?=TOTALPATH?>Servizi_immobiliare_casa_vacanze_residence_toscana.php" title="residence affitti estivi castiglioncello"><?php echo SERVIZI_FOOTER;?></a></li>
-                                        <li><a href="<?=LANFOLDER?>dove_immobiliare_casa_vacanze_toscana.php"><?php echo INFORMAZIONI_FOOTER;?></a></li>
-                                        <li><a href="<?=LANFOLDER?>pagamento.php"> <?php echo PAGAMENTI_FOOTER;?></a></li>
-                                    </ul>
-                                </div>
-
-                                <div class="col-md-3 col-xs-6 bottommargin-sm widget_links">
-                                    <ul>
-                                        <li><a href="<?=LANFOLDER?>immobili.php"><?php echo PROPOSTE_FOOTER;?></a</li>
-                                        <li><a href="<?=LANFOLDER?>residence_castiglioncello_casa_vacanze_residences_toscana.php" title="Residence castiglioncello"><?php echo RESIDENCE_FOOTER;?></a></li>
-                                        <li><a href="<?=LANFOLDER?>intorno_castiglioncello_immobiliare_casa_vacanze_residence_toscana.php"><?php echo DINTORNI_FOOTER;?></a></li>
-                                        <li><a href="<?=LANFOLDER?>case-vacanze.php"><?php echo VACANZE_FOOTER;?></a></li>
-                                        <li><a href="<?=LANFOLDER?>castiglioncello_immobiliare_casa_vacanze_residence_toscana.php">Castiglioncello</a></li>
-                                    </ul>
-                                </div>
-
-                                <div class="col-md-3 col-xs-6 bottommargin-sm widget_links">
-                                    <ul>
-                                        <li><a href="<?=LANFOLDER?>residence_castiglioncello_casa_vacanze_residences_toscana.php"> <?php echo RESIDENCESULMARE_FOOTER;?></a></li>
-                                        <li><a href="<?=LANFOLDER?>landing1.php" title="Residence Castiglioncello">Residence Castiglioncello</a></li>
-                                    </ul>
-                                </div>
-
-                                <div class="col-md-3 col-xs-6 bottommargin-sm widget_links">
-                                    <ul>
-                                        <li><a href="<?=LANFOLDER?>privacy-disclaimer_casa_vacanze_residence_toscana.php"> <?php echo PRIVACY_FOOTER;?></a></li>
-                                        <li><a href="informativa-estesa-cookie.php">Cookie Policy</a></li>
-                                    </ul>
-                                </div>
-
-                            </div>
-
-                        </div>
-
+                    <div class="col_two_fifth" data-class-lg="tleft" data-class-md="tleft" data-class-sm="tleft" data-class-xs="center" data-class-xxs="center">
+                        <img src="images/custom/footer-logo.png" alt="Radar logo">
                     </div>
-
-                    <div class="col_one_third col_last">
-
-                        <div class="widget clearfix" style="margin-bottom: -20px;">
-                            <div class="row">
-                                <div class="col-md-6 bottommargin-sm">
-                                    <img src="images/custom/fiaip.jpg">
-                                </div>
-                                <div class="col-md-6 bottommargin-sm">
-                                    <img src="images/custom/bandiera-blu.png">
-                                </div>
-                            </div>
-                        </div>
-
-
+                    <div class="col_three_fifth col_last" data-class-lg="tleft" data-class-md="tleft" data-class-sm="tleft" data-class-xs="center" data-class-xxs="center">
+                        <p><strong>Agenzia Radar Immobiliare</strong> - Via Aurelia, 548 - 57016 Castiglioncello (LI)<br>Tel. +39.0586.752596 - Fax +39.0586.759935 - Email: <a href="info@agenziaradar.it">info@agenziaradar.it</a><br>P.iva 01169600499</p>
                     </div>
+                    
+                    <div class="line" style="margin: 30px 0;"></div>
 
-                </div><!-- .footer-widgets-wrap end -->
+                    <div class="row"  data-class-lg="tleft" data-class-md="tleft" data-class-sm="tleft" data-class-xs="center" data-class-xxs="center">
 
-            </div>
-
-            <!-- Copyrights
-            ============================================= -->
-            <div id="copyrights">
-
-                <div class="container clearfix">
-
-                    <div class="col_half">
-                        <div class="copyrights-menu copyright-links clearfix">
-                            <a href="#">Home page</a>/<a href="#">Dove siamo</a>/<a href="#">Contatti</a>
+                        <div class="col-md-3 col-sm-6 col-xs-12 bottommargin-sm widget_links">
+                            <ul>
+                                <li><a class="home" href="<?=LANFOLDER?>index.php"><?php echo HOME_FOOTER; ?></a></li>
+                                <li><a href="<?=LANFOLDER?>agenzia_immobiliare_casa_vacanze_toscana.php" title="affitti case vacanze"><?php echo AGENZIA_FOOTER; ?></a></li>
+                                <li><a href="<?=TOTALPATH?>Servizi_immobiliare_casa_vacanze_residence_toscana.php" title="residence affitti estivi castiglioncello"><?php echo SERVIZI_FOOTER;?></a></li>
+                                <li><a href="<?=LANFOLDER?>dove_immobiliare_casa_vacanze_toscana.php"><?php echo INFORMAZIONI_FOOTER;?></a></li>
+                                <li><a href="<?=LANFOLDER?>pagamento.php"> <?php echo PAGAMENTI_FOOTER;?></a></li>
+                            </ul>
                         </div>
-                        Copyrights &copy; <?=date('Y')?> - Tutti i diritti riservati Agenzia Radar Srl.
-                    </div>
 
-                    <div class="col_half col_last tright">
-                        <div class="fright clearfix">
-                            <a href="#" class="social-icon si-small si-borderless nobottommargin si-facebook">
-                                <i class="icon-facebook"></i>
-                                <i class="icon-facebook"></i>
-                            </a>
-
-                            <a href="#" class="social-icon si-small si-borderless nobottommargin si-twitter">
-                                <i class="icon-twitter"></i>
-                                <i class="icon-twitter"></i>
-                            </a>
-
-                            <a href="#" class="social-icon si-small si-borderless nobottommargin si-youtube">
-                                <i class="icon-youtube"></i>
-                                <i class="icon-youtube"></i>
-                            </a>
-                            
+                        <div class="col-md-3 col-sm-6 col-xs-12 bottommargin-sm widget_links">
+                            <ul>
+                                <li><a href="<?=LANFOLDER?>immobili.php"><?php echo PROPOSTE_FOOTER;?></a</li>
+                                <li><a href="<?=LANFOLDER?>residence_castiglioncello_casa_vacanze_residences_toscana.php" title="Residence castiglioncello"><?php echo RESIDENCE_FOOTER;?></a></li>
+                                <li><a href="<?=LANFOLDER?>intorno_castiglioncello_immobiliare_casa_vacanze_residence_toscana.php"><?php echo DINTORNI_FOOTER;?></a></li>
+                                <li><a href="<?=LANFOLDER?>case-vacanze.php"><?php echo VACANZE_FOOTER;?></a></li>
+                                <li><a href="<?=LANFOLDER?>castiglioncello_immobiliare_casa_vacanze_residence_toscana.php">Castiglioncello</a></li>
+                            </ul>
                         </div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12 bottommargin-sm widget_links">
+                            <ul>
+                                <li><a href="<?=LANFOLDER?>residence_castiglioncello_casa_vacanze_residences_toscana.php"> <?php echo RESIDENCESULMARE_FOOTER;?></a></li>
+                                <li><a href="<?=LANFOLDER?>landing1.php" title="Residence Castiglioncello">Residence Castiglioncello</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="col-md-3 col-sm-6 col-xs-12 bottommargin-sm widget_links">
+                            <ul>
+                                <li><a href="<?=LANFOLDER?>privacy-disclaimer_casa_vacanze_residence_toscana.php"> <?php echo PRIVACY_FOOTER;?></a></li>
+                                <li><a href="informativa-estesa-cookie.php">Cookie Policy</a></li>
+                            </ul>
+                        </div>
+
                     </div>
 
                 </div>
 
-            </div><!-- #copyrights end -->
+            </div>
 
-        </footer><!-- #footer end -->
+            <div class="col_one_third col_last" data-class-lg="tleft" data-class-md="tleft" data-class-sm="tleft" data-class-xs="center" data-class-xxs="center">
 
-	</div><!-- #wrapper end -->
+                <div class="widget clearfix" style="margin-bottom: -20px;">
+                    <div class="row">
+                        <div class="col-md-6 bottommargin-sm">
+                            <img src="images/custom/fiaip.jpg">
+                        </div>
+                        <div class="col-md-6 bottommargin-sm">
+                            <img src="images/custom/bandiera-blu.png">
+                        </div>
+                    </div>
+                </div>
 
-	<!-- Go To Top
-	============================================= -->
-	<div id="gotoTop" class="icon-angle-up"></div>
 
-	<!-- Footer Scripts
-	============================================= -->
-	<script type="text/javascript" src="js/functions.js"></script>
+            </div>
+
+        </div><!-- .footer-widgets-wrap end -->
+
+    </div>
+
+    <!-- Copyrights
+    ============================================= -->
+    <div id="copyrights">
+
+        <div class="container clearfix">
+
+            <div class="col_half">
+                Copyrights &copy; <?=date('Y')?> - Tutti i diritti riservati Agenzia Radar Srl.
+            </div>
+
+            <div class="col_half col_last tright">
+                <div class="fright clearfix">
+                    <a href="#" class="social-icon si-small si-borderless nobottommargin si-facebook">
+                        <i class="icon-facebook"></i>
+                        <i class="icon-facebook"></i>
+                    </a>
+
+                    <a href="#" class="social-icon si-small si-borderless nobottommargin si-twitter">
+                        <i class="icon-twitter"></i>
+                        <i class="icon-twitter"></i>
+                    </a>
+
+                    <a href="#" class="social-icon si-small si-borderless nobottommargin si-youtube">
+                        <i class="icon-youtube"></i>
+                        <i class="icon-youtube"></i>
+                    </a>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div><!-- #copyrights end -->
+
+</footer><!-- #footer end -->
+
+</div><!-- #wrapper end -->
+
+<!-- Go To Top
+============================================= -->
+<div id="gotoTop" class="icon-angle-up"></div>
+
+<!-- Footer Scripts
+============================================= -->
+<script type="text/javascript" src="js/functions.js"></script>
 
 
 
 
 <div align="center">
-	<!-- Inizio Codice Shinystat -->
-     <script type="text/javascript" src="http://codice.shinystat.com/cgi-bin/getcod.cgi?USER=cogiorgio"> </script>
-		<noscript>
-			<a href="http://www.shinystat.com/it" target="_top">
-			<img src="http://www.shinystat.com/cgi-bin/shinystat.cgi?USER=cogiorgio" alt="Contatore siti" border="0"></a>
-		</noscript>
-		<!-- Fine Codice Shinystat --></div>
+    <!-- Inizio Codice Shinystat -->
+    <script type="text/javascript" src="http://codice.shinystat.com/cgi-bin/getcod.cgi?USER=cogiorgio"> </script>
+    <noscript>
+        <a href="http://www.shinystat.com/it" target="_top">
+        <img src="http://www.shinystat.com/cgi-bin/shinystat.cgi?USER=cogiorgio" alt="Contatore siti" border="0"></a>
+    </noscript>
+    <!-- Fine Codice Shinystat -->
+</div>
